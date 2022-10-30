ed6459e636b76bafce0bf11bcc8ac024ace6ee82
hemachandiran1102 <76897910+hemachandiran1102@users.noreply.github.com>
2022-10-30 11:43:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
+app.logger.disabled = True
+    log = logging.getLogger('werkzeug')
+    log.disabled = True
+    logging.getLogger('werkzeug').disabled = True
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
